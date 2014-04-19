d471e89032b27899d1623eee9aad68a19529a4ad
Dwayne Macgowan <dwaynemac@gmail.com>
2014-04-19 14:47:58 -0300

    [#69629336] methods to change task state

--- app/models/planning_client/to_do.rb
+++ app/models/planning_client/to_do.rb
@@ -21,5 +21,48 @@ module PlanningClient
     attribute :due_at
 
     attribute :account_name
+
+    # mark ToDo as in progress
+    def start
+      if self.status_transition("start")
+        self.state = 'in_progress'
+        self
+      else
+        nil
+      end
+    end
+   
+    # mark ToDo as in stand by
+    def hold
+      if self.status_transition("hold")
+        self.state = 'stand_by'
+        self
+      else
+        nil
+      end
+    end
+   
+    # mark ToDo as completed
+    def complete
+      if self.status_transition("complete")
+        self.state = 'completed'
+        self
+      else
+        nil
+      end
+    end
+   
+    protected
+
+    def status_transition(transition)
+      response = Typhoeus::Request.get( "#{self.class.resource_uri(id)}/#{transition}", params: self.class.merge_key({}) )
+      if response.code == 200
+        log_ok(response)
+        return self
+      else
+        log_failed(response)
+        return nil
+      end
+    end
   end
 end
