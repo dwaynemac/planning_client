9d7b376c0b0e276e96691452aa83587e1b350b25
Sebastian Reca <sebareca@gmail.com>
2015-06-02 19:47:49 -0300

    [#93568854] there is no need for this now

--- lib/planning_client/service_configuration.rb
+++ lib/planning_client/service_configuration.rb
@@ -3,14 +3,6 @@ module PlanningClient
     def self.included(base)
       base.send('use_hydra', PlanningClient::HYDRA)
 
-      host = case ENV['RACK_ENV']
-        when 'development'
-          PlanningClient::HOST
-        when 'production'
-          'padma-planning.heroku.com'
-      end
-
-      base.send('set_resource_host', host)
       base.send('configure_index_response', {collection: 'collection', total: 'total'})
 
       base.send('set_api_key','app_key',ENV['planning_api_key'])
