236e6cf32ac20248ca3e7f84a5995b19abafbecb
Sebastian Reca <sebareca@gmail.com>
2015-06-08 14:08:59 -0300

    [#93568854] Change set_email_options method for a complete update
    Also it only does the update is necessary

--- app/models/planning_client/user.rb
+++ app/models/planning_client/user.rb
@@ -10,10 +10,30 @@ module PlanningClient
     attribute :id
     attribute :daily_email_reminder_enabled
     attribute :username
+    attribute :email
 
-    def set_email_options(value)
-      if daily_email_reminder_enabled != value
-        self.daily_email_reminder_enabled = value
+    # Updates User only if necessary to avoid
+    # requests with no effect.
+    #
+    # @params [Hash] params
+    #   daily_email: Boolean
+    #   email: String
+    def update_if_necessary(params)
+      update_needed = false 
+      
+      if !params[:daily_email].nil? &&
+          self.daily_email_reminder_enabled != params[:daily_email]
+        update_needed = true
+        self.daily_email_reminder_enabled = params[:daily_email]
+      end
+
+      if !params[:email].nil? && 
+          self.email != params[:email]
+        update_needed = true
+        self.email = params[:email]
+      end
+
+      if update_needed
         self.save
       end
     end
